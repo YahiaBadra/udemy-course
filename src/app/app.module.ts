ec441a1938532f4dd0e285f81b1a52d9ab350a7f
Mohamed Yahya Badra <m.badra@riekerworld.com>
2023-11-22 08:25:29 +0100

    add recipes modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthComponent } from './auth/auth.component';
 import { HeaderComponent } from './header/header.component';
-import RecipeBookComponent from './recipe-book/recipe-book.component';
 import { RecipeBookService } from './recipe-book/recipe-book.service';
-import { RecipeDetailComponent } from './recipe-book/recipe-detail/recipe-detail.component';
-import { RecipeEditComponent } from './recipe-book/recipe-edit/recipe-edit.component';
-import { RecipeItemComponent } from './recipe-book/recipe-item/recipe-item.component';
-import { RecipeListComponent } from './recipe-book/recipe-list/recipe-list.component';
-import { RecipeStartComponent } from './recipe-book/recipe-start/recipe-start.component';
 import { AlertComponent } from './shared/alert/alert.component';
 import { DropdownDirective } from './shared/dropdown.directive';
 import { LoadingComponent } from './shared/loading/loading.component';
@@ -21,22 +15,17 @@ import { PlaceholderDirective } from './shared/placeholder/placeholder.directive
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
+import { RecipesModule } from './recipe-book/recipes.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     ShoppingListComponent,
-    RecipeBookComponent,
     ShoppingEditComponent,
-    RecipeDetailComponent,
-    RecipeListComponent,
-    RecipeItemComponent,
     DropdownDirective,
     AuthComponent,
     LoadingComponent,
-    RecipeStartComponent,
-    RecipeEditComponent,
     AlertComponent,
     PlaceholderDirective,
     AlertComponent,
@@ -47,9 +36,9 @@ import { ShoppingListService } from './shopping-list/shopping-list.service';
     FormsModule,
     HttpClientModule,
     ReactiveFormsModule,
+    RecipesModule,
   ],
   providers: [ShoppingListService, RecipeBookService],
   bootstrap: [AppComponent],
-  
 })
 export class AppModule {}
---
+++ src/app/recipe-book/recipes.module.ts
@@ -0,0 +1,36 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import RecipeBookComponent from './recipe-book.component';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeItemComponent } from './recipe-item/recipe-item.component';
+import { RecipeStartComponent } from './recipe-start/recipe-start.component';
+import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    RecipeBookComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeEditComponent,
+    RecipeStartComponent,
+    RecipeEditComponent,
+  ],
+  imports:[RouterModule , CommonModule , ReactiveFormsModule],
+  exports:[
+    RecipeBookComponent,
+    RecipeDetailComponent,
+    RecipeListComponent,
+    RecipeItemComponent,
+    RecipeStartComponent,
+    RecipeEditComponent,
+  ],
+})
+export class RecipesModule {}
